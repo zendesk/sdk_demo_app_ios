9fe3e564aa585aea644e26b8a4d2c4b8a5e6a6da
Ronan Mchugh <rmchugh@zendesk.com>
2019-06-28 15:37:07 +0100

    Release 2.1.0 of RTD with Answer Bot 1.0.0 and Support 3.0.0

--- Remember The Date/View Controllers/ZenHelpViewController.h
+++ Remember The Date/View Controllers/ZenHelpViewController.h
@@ -13,6 +13,7 @@
 @property (weak, nonatomic) IBOutlet UIButton *contactUsButton;
 @property (weak, nonatomic) IBOutlet UIButton *myTicketsButton;
 @property (weak, nonatomic) IBOutlet UIButton *startChatButton;
+@property (weak, nonatomic) IBOutlet UIButton *answerBotbutton;
 @property (weak, nonatomic) IBOutlet UILabel *topLabel;
 
 @end
