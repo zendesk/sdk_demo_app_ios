257805e278dd10e566b398f1bb65ad521c1d5c7a
Killian Smith <kismith@zendesk.com>
2020-03-05 16:00:51 +0000

    1.4.7 artefacts

--- Remember The Date/View Controllers/ZenHelpViewController.m
+++ Remember The Date/View Controllers/ZenHelpViewController.m
@@ -14,6 +14,7 @@
 #import <MessagingSDK/MessagingSDK.h>
 
 #import <ZDCChat/ZDCChat.h>
+#import <ZDCChatAPI/ZDCChatAPI.h>
 #import "ZenHelpViewController.h"
 #import "SaveTheDateTabBarController.h"
 
